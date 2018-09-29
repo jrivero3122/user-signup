160a35e6ce2d7af44cd07cec1f8c838ed3cca5a9
Jorge Rivero <jrivero3122@gmail.com>
2018-09-29 15:23:07 -0400

    Validation empty fields ok, empty space ok, len of string ok

--- main.py
+++ main.py
@@ -14,21 +14,25 @@ def index():
     template = jinja_env.get_template('index.html')
     return template.render()
 
-@app.route("/", methods=['POST'])
+@app.route("/", methods=['POST', 'GET'])
 def signup():
     username = request.form['username']
     password = request.form['password']
     verifyPassword = request.form['verifyPassword']
-    # email = request.form['email']
+    email = request.form['email']
 
     if username and password and verifyPassword:
-        template = jinja_env.get_template('hello.html') 
-        # if (len(username)<20 and len(username)>3 and (username.isspace() == True)):
+        if (len(username)<20 and len(username)>3 and (' ' in username) == False) and (len(password)<20 and len(password)>3 and (' ' in password) == False) and (len(verifyPassword)<20 and len(verifyPassword)>3 and (' ' in verifyPassword) == False):
+            template = jinja_env.get_template('hello.html') 
+        # else:
+        #     template = jinja_env.get_template('index.html') 
+        #     warning1 = "Fields empty"
                    
        
-    return template.render(name=username, passw=password, veri=verifyPassword, email=email)
+    return template.render(name=username, passw=password, veri=verifyPassword, email=email, )
 
 
 app.run()
 
     #    if (len(username)<20 and len(username)>3 and (username.isspace() == True)) and (len(password)<20 and len(password)>3 and (password.isspace() == True)) and (len(verifyPassword)<20 and len(verifyPassword)>3 and (verifyPassword.isspace() == True)):
+    # warning1=warning1
